7b87dd77595cbbda6546bddfbadd2bc6fd99c139
Zheyu <oranz2049@gmail.com>
2022-07-30 16:21:08 +0200

    Update train_pose2vid.py

--- train_pose2vid.py
+++ train_pose2vid.py
@@ -79,7 +79,7 @@ def main():
             ############## Display results and errors ##########
             ### print out errors
             if total_steps % opt.print_freq == print_delta:
-                errors = {k: v.data[0] if not isinstance(v, int) else v for k, v in loss_dict.items()}
+                errors = {k: v.item() if not isinstance(v, int) else v for k, v in loss_dict.items()}
                 t = (time.time() - iter_start_time) / opt.batchSize
                 visualizer.print_current_errors(epoch, epoch_iter, errors, t)
                 visualizer.plot_current_errors(errors, total_steps)
